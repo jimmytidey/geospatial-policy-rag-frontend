81b3cb16f15f187b9af47afe80b93cf628bca790
jimmytidey <jimmytidey@gmail.com>
2024-10-29 00:00:42 +0000

    changing build folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig,loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  // Load app-level env vars to node-level env vars.
+  const env = loadEnv(mode, process.cwd());
+
+  // You don't need to use `module.exports` in Vite with ES module syntax.
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'build', // Output directory for the build
+      sourcemap: true,  // Enable sourcemaps for production builds
+    },
+  };
+});
